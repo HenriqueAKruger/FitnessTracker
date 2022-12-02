f7ad7289de050ecc4ca8cc37a81cba73c8f7e4a8
HenriqueAdamiKruger <henrique.adami@unidavi.edu.br>
2022-12-02 12:28:37 -0300

    Updating project

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -66,28 +66,9 @@ class ImcActivity : AppCompatActivity() {
             imc < 40.0 -> R.string.imc_severely_high_weight
             else -> R.string.imc_extreme_weight
         }
-//        if (imc < 15.0) {
-//            return R.string.imc_severely_low_weight
-//        } else if (imc < 16.0) {
-//            return R.string.imc_very_low_weight
-//        } else if (imc < 18.5) {
-//            return R.string.imc_low_weight
-//        } else if (imc < 25.0) {
-//            return R.string.normal
-//        } else if (imc < 30.0) {
-//            return R.string.imc_high_weight
-//        } else if (imc < 35.0) {
-//            return R.string.imc_so_high_weight
-//        } else if (imc < 40.0) {
-//            return R.string.imc_severely_high_weight
-//        } else {
-//            return R.string.imc_extreme_weight
-//        }
-
     }
 
     private fun calculateImc(weight: Int, height: Int): Double {
-        // peso / (altura * altura)
         return weight / ( (height / 100.0) * (height / 100.0) )
     }
 
--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,42 +1,24 @@
 package co.tiagoaguiar.fitnesstracker
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var btnImc: LinearLayout
     private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1) o layout XML
-        // 2) a onde a recyclerview vai aparecer (tela principal, tela cheia)
-        // 3) logica - conectar o xml da celula DENTRO do recyclerView + a sua quantidade de elementos dinamicos
         val adapter = MainAdapter()
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
         rvMain.layoutManager = LinearLayoutManager(this)
-
-        // classe para administar a recyclerview e suas celulas (os seus layouts de itens)
-        // Adapter ->
-
-//        btnImc = findViewById(R.id.btn_imc)
-//
-//        btnImc.setOnClickListener {
-//            // navegar para proxima tela
-//            val i = Intent(this, ImcActivity::class.java)
-//            startActivity(i)
-//        }
     }
 
     private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
@@ -47,8 +29,7 @@ class MainActivity : AppCompatActivity() {
             return MainViewHolder(view)
         }
 
-        // 2 - disparado toda vez houver uma rolagem na tela e for necessario trocar o conteudo
-        // da celula
+        // 2 - disparado toda vez houver uma rolagem na tela e for necessario trocar o conteudo da celula
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         }
 
